4075baa3c396ace0a51c1ba0f41b9d3b4b514e39
Darkcoffe1859 <shency49@gmail.com>
2023-08-03 08:49:22 +0100

    tree leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -3,11 +3,11 @@
  * binary_tree_leaves - function that returns the number of leaves in a tree
  * @tree: tree to check
  * Return: number of leaves
- *
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaf = 0, l = 0, r = 0;
+	
 	if (tree == NULL)
 	{
 		return (0);
@@ -16,6 +16,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		l = binary_tree_leaves(tree->left);
 		r = binary_tree_leaves(tree->right);
+		leaf = l + r;
 		return ((!l && !r) ? leaf + 1 : leaf + 0);
 	}
 }
